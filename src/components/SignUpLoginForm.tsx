e06ca443f74078ef66a6f880edeb8bbbf51da7fa
jossysite <106884670+yohannes2014@users.noreply.github.com>
2025-02-10 14:55:27 +0300

    Client side update for production

--- src/components/SignUpLoginForm.tsx
+++ src/components/SignUpLoginForm.tsx
@@ -12,12 +12,7 @@ const SignUpLoginForm:React.FC = () => {
   
   const formUser = useSelector((state:RootState)=>state.users.logSignForm);
   const form = useSelector((state:RootState)=>state.users.Form)
-  const dispatch = useDispatch()
-  
-
- 
- 
-
+  const dispatch = useDispatch();
   const handleLogin = () => {
     dispatch(setForm('login'))
   }
@@ -27,15 +22,13 @@ const SignUpLoginForm:React.FC = () => {
  
 
 
-
-
   return (
     <>
      {formUser && 
-     <div className='fixed w-screen m-auto h-screen flex justify-center items-center bg-[rgba(205, 209, 234, 0.681)] backdrop-blur-[4px]'>
+     <div className='fixed w-screen m-auto h-screen flex justify-center  bg-[rgba(205, 209, 234, 0.681)] backdrop-blur-[4px]'>
 
          
-     <div  className= 'border-2 bg-white top-32m-auto w-full md:w-[600px] mx-4 shadow rounded-lg p-4'>
+     <div  className= 'border-2 bg-white top-32m-auto h-fit mt-32 md:mt-40 w-full md:w-[600px] mx-4 shadow rounded-lg p-4'>
       <Note />
            <div className="mb-2 flex gap-2">
          <span onClick={handleLogin} className={`w-full cursor-pointer hover:bg-yellow-100 text-center text-primary border-2 font-bold text-lg ${form === 'login'&& 'bg-yellow-100'} `}>Login</span>
